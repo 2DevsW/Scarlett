17d753dd101a382857bf73fa96b12b4aa94112cc
Ice <88745683+IceK1203@users.noreply.github.com>
2023-06-11 12:12:26 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-DiscordAPI = "MTEwMTk2NTMzNTU5NjM3NjA4NQ.GmYC1j.3IeWlLoKukyfBbi78hIdE47j4xI-FLW1Omi8j8"
-APIkey = "b6001874-eb2e-4cdf-9835-270d059d0c85"
-UIName = 'scarlett'
-Prefix = '//'
-Version = '1.6.5'
+DiscordAPI = ''
+APIkey = ''
+UIName = ''
+Prefix = ''
+Version = ''
